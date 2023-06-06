e1e7d92f14fd0fbd4bd8ca6ba2d17f799da461b2
bruno santos <devbrunosantos@gmail.com>
2023-06-06 09:56:59 -0300

    feat: images with alt

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,3 @@
-// import { List } from "../assets/icons";
 import Image from "next/image";
 
 import logo from "../../public/logo.svg";
@@ -7,10 +6,10 @@ import numberZero from "../../public/number-0.svg";
 export function Header() {
   return (
     <header className="fixed left-0 right-0 top-0 z-50 px-20 py-10 flex items-center justify-between pointer-events-none">
-      <Image src={logo} alt="dddd" height={32} />
+      <Image src={logo} alt="Bruno's logo" height={32} />
 
       <button className="pointer-events-auto rounded-full">
-        <Image src={numberZero} alt="dqdq" height={32} />
+        <Image src={numberZero} alt="Button for open menu" height={32} />
       </button>
     </header>
   );
