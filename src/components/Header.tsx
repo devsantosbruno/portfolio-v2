03e3eeeac94947d24bcdca74ca6a6df67cc35590
bruno santos <devbrunosantos@gmail.com>
2023-06-18 19:36:30 -0300

    feat: Header component without menu mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,11 @@
 import Image from "next/image";
 
 import logo from "../../public/logo.svg";
-import numberZero from "../../public/number-0.svg";
 
 export function Header() {
   return (
-    <header className="fixed left-0 right-0 top-0 z-50 px-20 py-10 flex items-center justify-between pointer-events-none">
+    <header className="fixed left-0 right-0 top-0 z-40 px-20 py-10 pointer-events-none">
       <Image src={logo} alt="Bruno's logo" height={32} />
-
-      <button className="pointer-events-auto rounded-full">
-        <Image src={numberZero} alt="Button for open menu" height={32} />
-      </button>
     </header>
   );
 }
